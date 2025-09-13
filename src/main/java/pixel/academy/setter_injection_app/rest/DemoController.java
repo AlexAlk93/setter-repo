fbc670889455ef69188369ec417e8ceac6a9e3fb
AlexAlk93 <alex.panioglo@gmail.com>
2025-09-13 17:48:55 +0300

    Initial commit

--- src/main/java/pixel/academy/setter_injection_app/rest/DemoController.java
+++ src/main/java/pixel/academy/setter_injection_app/rest/DemoController.java
@@ -1,18 +1,19 @@
 package pixel.academy.setter_injection_app.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pixel.academy.setter_injection_app.common.Chef;
-
 
 @RestController
 public class DemoController {
     private Chef myChef;
 
-    //Contructor
+    //Setter
     @Autowired
-   public  void setChef(Chef theChef){
+   public  void setChef( Chef theChef){
         myChef = theChef;
     }
     @GetMapping("/dailyrecipe")
